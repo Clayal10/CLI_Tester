dc68843adfeadf5feb3202813acdf87458a40c43
Clay Shumaker <clayalshu@gmail.com>
2025-01-18 20:11:54 -0800

    Stupid bug with vectors, but it's probably my fault

--- cli_test.hpp
+++ cli_test.hpp
@@ -1,13 +1,14 @@
 //To do:
 //	I need a way to access the returns of the methods in test.c in a nice user friendly way.
-//		
+//	Try this:
+//		push back function pointers that take a CLI method return in as a parameter
+//
 
 #ifndef CLI_TEST_HPP
 #define CLI_TEST_HPP
 
 #include<iostream>
 #include<vector>
-
 /****Colored text macros****/
 #define COLOR_RED "\x1b[31m"
 #define COLOR_GREEN "\x1b[32m"
@@ -15,12 +16,11 @@
 
 
 //list of function pointers. Functions will be the CLI class methods
-typedef std::vector<void (*)()> cli_test_list;
-typedef void* TEST;
+typedef void TEST;
 //For easy access in test.cpp
 namespace cli_test{
-	inline cli_test_list unit_tests;
 	inline int testing_mode = 0;
+	std::vector<void(*)()> unit_tests;
 }
 
 
@@ -52,6 +52,7 @@ inline int init_testing_mode(int argc, char** argv);
 //Called from init_testing_mode TODO something
 //I need to decide what to do with the rest of the code in main if we are in test mode. It could still be ran or it could be skipped over some how?
 inline void run_tests();
+
 
 /****Function Definitions****/ //may move to a seperate file eventually.
 int init_testing_mode(int argc, char** argv){
--- cpp_test_example/test.cpp
+++ cpp_test_example/test.cpp
@@ -1,5 +1,13 @@
 #include "../cli_test.hpp"
 #include "example.hpp"
 
+TEST test1(){
+	CLI::is_equal(2, 2);
+}
+cli_test::unit_tests.push_back(&test1);
 
-
+TEST test2(){
+	CLI::is_equal(1, 2);
+	CLI::is_false(0);
+}
+cli_test::unit_tests.push_back(&test2);
