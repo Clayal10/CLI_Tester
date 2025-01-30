583512b8597c4c7f4fc0269ec621bfd7d156da59
Clay Shumaker <clayalshu@gmail.com>
2025-01-30 07:57:15 -0800

    Started creating a unit test header file written in C.

--- c_test_example/main.c
+++ c_test_example/main.c
@@ -5,6 +5,9 @@
 int main(int argc, char** argv){
 	/*Call this function at the start of main()*/
 	init_testing_mode(argc, argv);
-	
+	test_main();
+
+	puts("In Main");
+
 	return 0;
 }
---
+++ cli_test.h
@@ -0,0 +1,55 @@
+#ifndef CLI_TEST_H
+#define CLI_TEST_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_RESET "\x1b[0m"
+
+//Prototypes
+void test_main();
+static inline void init_testing_mode(int argc, char** argv);
+static inline void run_tests();
+
+//Objects
+/*
+ * Ultimately, I need a list of boolean values for each unit test.
+ * The cpp version uses a macro to have each unit test push back a list of booleans as returns from class methods.
+ * We could do the following:
+ * 	- Use a struct of functions for our boolean return methods, similar to a class.
+ * 	- Could us va_args (see if we can avoid)
+ * 		- Have a list of size 2, index 0 being test name and index 1 being a list of booleans.
+ * 	- Use some sort of dynamically sized list for all the unit tests, esentially create our own std::vector.
+ * */
+
+
+void init_testing_mode(int argc, char** argv){
+	if(argc <= 1){
+		return;
+	}
+	char test_flag[7] = "--test";
+	for(int i=0; i<6; i++){
+		if(argv[1][i] == '\0'){
+			return;
+		}
+		if(argv[1][i] != test_flag[i]){
+			return;
+		}
+	}
+	run_tests();
+}
+
+
+void run_tests(){
+	
+	
+	puts(COLOR_GREEN "Testing" COLOR_RESET);
+
+	exit(0);
+
+}
+
+#endif
+
